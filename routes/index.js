36d6c7f3d5606276f948bbfb75cabaed9cc883cb
rb-isiaq <isiaqridwanbukola1999@gmail.com>
2024-07-01 14:23:12 +0100

    changed app to index

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ const { getLocationTemp } = require("../controllers");
 const { validateVisitorName } = require("../validators");
 const router = express.Router();
 
-router.get("/", function (req, res, next) {
-  res.send({
-    title: "Location API",
-    message: "make use of /api/hello?visitor_name route",
-  });
-});
+// router.get("/", function (req, res, next) {
+//   res.send({
+//     title: "Location API",
+//     message: "make use of /api/hello?visitor_name route",
+//   });
+// });
 router.get("/api/hello", validateVisitorName, getLocationTemp);
 
 module.exports = router;
